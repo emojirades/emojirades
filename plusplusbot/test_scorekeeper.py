c4d4268fd8b48d42af4b9d110c1163b3b93941ef
Michael Robbins <michael.robbins@nebulr.com.au>
2017-12-31 12:17:04 +1100

    Fixes to tests

--- plusplusbot/test_scorekeeper.py
+++ plusplusbot/test_scorekeeper.py
@@ -5,15 +5,16 @@ import csv
 
 
 def test_new_file_load():
-    with tempfile.NamedTemporaryFile() as temp_file:
+    with tempfile.NamedTemporaryFile(mode="wt", newline="") as temp_file:
         keeper = ScoreKeeper(filename=temp_file.name)
 
         assert len(keeper.scoreboard.keys()) == 0
 
 def test_existing_file_load():
-    with tempfile.NamedTemporaryFile() as temp_file:
+    with tempfile.NamedTemporaryFile(mode="wt", newline="") as temp_file:
         writer = csv.writer(temp_file, delimiter=',')
-        writer.writerow(['U12345', '10'])
+        writer.writerow(["U12345", "10"])
+        temp_file.flush()
 
         keeper = ScoreKeeper(filename=temp_file.name)
 
