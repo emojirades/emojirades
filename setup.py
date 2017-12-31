0499e71b7b7951bbd15bc40c8f36347f285a6f5e
Michael Robbins <michael.robbins@nebulr.com.au>
2017-12-31 13:41:17 +1100

    Fixing up setup.py and package versioning for pypi packaging

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Emojirade PlusPlus",
+    name="Emojirades PlusPlus",
     version="0.0.1",
+    description="A Slack bot that understands the Emojirades game!",
+    url="https://github.com/michael-robbins/emojirades-plusplus",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Communications :: Chat",
+        "Topic :: Games/Entertainment",
+    ],
+    keywords="slack slackbot emojirades",
     license="GNU",
     packages=["plusplusbot"],
-    scripts=["bin/main.py"]
+    install_requires=[
+        "slackclient>=1.1,<2",
+    ],
+    python_requires="~=3.6",
+    extra_requires={
+        "test": ["pytest>=3.3,<4"],
+    },
+    scripts=["bin/emojirades-plusplus"]
 )
