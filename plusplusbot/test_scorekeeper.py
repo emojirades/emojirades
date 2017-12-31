c97b07de860aa8ad12884715681032b3bd3f6bc7
Michael Robbins <michael.robbins@nebulr.com.au>
2017-12-31 12:07:32 +1100

    Moving writer into file context

--- plusplusbot/test_scorekeeper.py
+++ plusplusbot/test_scorekeeper.py
@@ -11,9 +11,8 @@ def test_new_file_load():
         assert len(keeper.scoreboard.keys()) == 0
 
 def test_existing_file_load():
-    writer = csv.writer(delimiter=',')
-
     with tempfile.NamedTemporaryFile() as temp_file:
+        writer = csv.writer(temp_file, delimiter=',')
         writer.writerow(['U12345', '10'])
 
         keeper = ScoreKeeper(filename=temp_file.name)
