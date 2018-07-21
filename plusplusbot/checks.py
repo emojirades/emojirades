8ed8487e608aea6a1896282d9e61f2945d0b1e56
Michael Robbins <michael@dalmura.com.au>
2018-07-21 15:58:52 +1000

    Disallow emojis in 'rade (#55)
    * Ability to disallow an emojirade based on a list of regexes
    * Disallow raw emojis in the 'rade
    * New test to check emojirade banning

---
+++ plusplusbot/checks.py
@@ -0,0 +1,10 @@
+import re
+
+# 'rade regexes that are banned
+banned_emojirades = [
+    re.compile(":[a-zA-Z-_]+:"),  # User is now allowed to include emojis
+]
+
+
+def emojirade_is_banned(emojirade):
+    return any(i.search(emojirade) for i in banned_emojirades)
