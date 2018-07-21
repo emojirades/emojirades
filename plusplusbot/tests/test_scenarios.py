8552668521f7b63fdbdfcbda834325e514f0e7d3
Michael Robbins <michael@dalmura.com.au>
2018-07-21 15:18:12 +1000

    Feed back the user who sent the 'rade, also overhaul the tests checking expected responses (#54)

--- plusplusbot/tests/test_scenarios.py
+++ plusplusbot/tests/test_scenarios.py
@@ -1,5 +1,7 @@
 from plusplusbot.tests.helper import EmojiradeBotTester
 
+import re
+
 
 class TestBotScenarios(EmojiradeBotTester):
     """
@@ -92,51 +94,84 @@ class TestBotScenarios(EmojiradeBotTester):
         Tests that the expected responses are bring returned
         Response order is asserted along with equality
         """
+        def response(dst, msg):
+            return (dst, re.compile(msg))
+
+        total_responses = 0
+
+        # Assert game hasn't started yet
         assert self.state["step"] == "new_game"
-        assert len(self.responses) == 0
+        assert len(self.responses) == total_responses
 
+        # User starts a new round manually
         self.send_event(self.events.new_game)
-        assert len(self.responses) == 4
 
+        # Expected *new* responses
         responses = [
-            (self.config.channel, "<@{0}> has set the old winner to <@{0}> and the winner to <@{1}>".format(self.config.player_1, self.config.player_2)),
-            (self.config.channel, "It's now <@{0}>'s turn to provide <@{1}> with the next 'rade!".format(self.config.player_1, self.config.player_2)),
-            (self.config.player_1_channel, "You'll now need to send me the new 'rade for <@{0}>".format(self.config.player_2)),
-            (self.config.player_1_channel, "Please reply back in the format `emojirade Point Break` if `Point Break` was the new 'rade"),
+            response(self.config.channel,
+                     "<@{0}> has set the old winner to <@{0}> and the winner to <@{1}>".format(self.config.player_1, self.config.player_2)),
+            response(self.config.channel,
+                     "It's now <@{0}>'s turn to provide <@{1}> with the next 'rade!".format(self.config.player_1, self.config.player_2)),
+            response(self.config.player_1_channel,
+                     "You'll now need to send me the new 'rade for <@{0}>".format(self.config.player_2)),
+            response(self.config.player_1_channel,
+                     "Please reply back in the format `emojirade Point Break` if `Point Break` was the new 'rade"),
         ]
 
+        # Ensure each expected response exists in actual responses
         for i, (channel, msg) in enumerate(responses):
             assert channel == self.responses[i][0]
-            assert msg in self.responses[i][1]
+            assert msg.match(self.responses[i][1])
 
+        # Ensure total volume is as expected
+        total_responses += len(responses)
+        assert len(self.responses) == total_responses
+
+        # User sends a 'rade
         self.send_event(self.events.posted_emojirade)
-        assert len(self.responses) == 6
 
+        # Expected *new* responses
         responses = [
-            (self.config.player_2_channel, "Hey, <@{0}> made the 'rade `{1}`, good luck!".format(self.config.player_1, self.config.emojirade)),
-            (self.config.channel, ":mailbox: 'rade sent to <@{1}>".format(self.config.player_1, self.config.player_2)),
+            response(self.config.player_1_channel,
+                     "Thanks for that! I've let {0} know!".format(self.config.player_2)),
+            response(self.config.player_2_channel,
+                     "Hey, <@{0}> made the 'rade `{1}`, good luck!".format(self.config.player_1, self.config.emojirade)),
+            response(self.config.channel,
+                     ":mailbox: 'rade sent to <@{1}>".format(self.config.player_1, self.config.player_2)),
         ]
 
+        # Ensure each expected response exists in actual responses
         for i, (channel, msg) in enumerate(responses):
-            assert channel == self.responses[4 + i][0]
-            assert msg in self.responses[4 + i][1]
+            assert channel == self.responses[total_responses + i][0]
+            assert msg.match(self.responses[total_responses + i][1])
 
+        # Ensure total volume is as expected
+        total_responses += len(responses)
+        assert len(self.responses) == total_responses
+
+        # User starts posting emoji's for the current round
         self.send_event(self.events.posted_emoji)
-        assert len(self.responses) == 6
+        assert len(self.responses) == total_responses  # Bot shouldn't respond
 
+        # User posts a correct guess
         self.send_event(self.events.correct_guess)
-        assert len(self.responses) == 10
 
+        # Expected *new* responses
         responses = [
-            (self.config.channel, "<@{0}>++".format(self.config.player_3)),
-            (self.config.channel, "Congrats <@{0}>, you're now at 1 point".format(self.config.player_3)),
-            (self.config.player_2_channel, "You'll now need to send me the new 'rade for <@{0}>".format(self.config.player_3)),
-            (self.config.player_2_channel, "Please reply back in the format `emojirade Point Break` if `Point Break` was the new 'rade"),
+            response(self.config.channel, "<@{0}>\\+\\+".format(self.config.player_3)),
+            response(self.config.channel, "Congrats <@{0}>, you're now at 1 point :[a-z_]+:".format(self.config.player_3)),
+            response(self.config.player_2_channel, "You'll now need to send me the new 'rade for <@{0}>".format(self.config.player_3)),
+            response(self.config.player_2_channel, "Please reply back in the format `emojirade Point Break` if `Point Break` was the new 'rade"),
         ]
 
+        # Ensure each expected response exists in actual responses
         for i, (channel, msg) in enumerate(responses):
-            assert channel == self.responses[6 + i][0]
-            assert msg in self.responses[6 + i][1]
+            assert channel == self.responses[total_responses + i][0]
+            assert msg.match(self.responses[total_responses + i][1])
+
+        # Ensure total volume is as expected
+        total_responses += len(responses)
+        assert len(self.responses) == total_responses
 
     def test_only_guess_when_guessing(self):
         """ Ensures we can only 'guess' correctly when state is guessing """
