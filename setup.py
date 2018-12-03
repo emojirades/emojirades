b21e317e6d55c6be80845e0b2438f7329c7630d5
Michael Robbins <michael@dalmura.com.au>
2018-12-03 21:28:22 +1100

    Bump to v0.4.0

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="Emojirades PlusPlus",
-    version="0.3.0",
+    version="0.4.0",
     description="A Slack bot that understands the Emojirades game!",
     url="https://github.com/michael-robbins/emojirades-plusplus",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Communications :: Chat",
         "Topic :: Games/Entertainment",
     ],
@@ -22,9 +21,9 @@ setup(
         "boto3>=1.5,<2",
         "Unidecode>=1,<2"
     ],
-    python_requires="~=3.5",
+    python_requires="~=3.7",
     extras_require={
-        "test": ["pytest>=3.3,<4"],
+        "test": ["pytest"],
     },
     scripts=["bin/emojirades-plusplus"]
 )
