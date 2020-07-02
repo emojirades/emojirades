0940ede5e4774215ef5fe316d194714d2dda40e4
phelios <phelio.xion@gmail.com>
2020-07-02 23:06:18 +1000

    Add history analyser for weekly and montly range score recalculation

--- plusplusbot/analytics/leaderboard.py
+++ plusplusbot/analytics/leaderboard.py
@@ -1,6 +1,99 @@
+# Thanks to https://github.com/BeheadedKamikaze for his help with the algorithm
+import math
+from collections import defaultdict
+
+import pendulum
+
+
 class LeaderBoard:
+
+    NOT_FOUND = -1
+
     def __init__(self, history):
         self.history = history
+        self.len = len(history)
 
-    def get_week(self):
-        return 100
+    # First step, find something in the history that matches the range.
+    def find_rows_in_range(self, start_time, end_time):
+        low = 0
+        high = self.len - 1
+
+        first_time = self.history[low]['timestamp']
+        last_time = self.history[high]['timestamp']
+
+        # Out of range
+        if end_time < first_time or start_time > last_time:
+            return self.NOT_FOUND
+
+        # history is empty
+        if low == high:
+            return low
+
+        # Binary search first guess with ratio to the time range of history
+        guess = math.floor((start_time - first_time) / (last_time - first_time) * (high - low) + low)
+
+        # Fine tune guess relative to the date range
+        while low <= high and low <= guess <= high:
+            if self.history[guess]['timestamp'] < start_time:
+                low = guess + 1
+            elif self.history[guess]['timestamp'] > end_time:
+                high = guess - 1
+            else:
+                return guess
+
+        # if somehow still find nothing
+        return self.NOT_FOUND
+
+    # Second step, flood-fill up and bottom
+    # this reduces search time compared to finding the upper/lower bound
+    def get_data(self, start_time, end_time):
+        results = []
+        affected_row_index = self.find_rows_in_range(start_time, end_time)
+
+        if affected_row_index == self.NOT_FOUND:
+            return results
+
+        results.append(self.history[affected_row_index])
+
+        # get events from start_time to checkpoint
+        cursor = affected_row_index - 1
+        while cursor >= 0 and self.history[cursor]['timestamp'] >= start_time:
+            results.insert(0, self.history[cursor])
+            cursor -= 1
+
+        # get events from checkpoint to end_time
+        cursor = affected_row_index + 1
+        while cursor <= (self.len - 1) and self.history[cursor]['timestamp'] <= end_time:
+            results.append(self.history[cursor])
+            cursor += 1
+
+        return results
+
+    @staticmethod
+    def calculate_score(history):
+        leaderboard = defaultdict(int)
+
+        for item in history:
+            val = 0
+            if item['operation'] == '++':
+                val = 1
+            elif item['operation'] == '--':
+                val = -1
+
+            leaderboard[item['user_id']] += val
+
+        return [{u: leaderboard[u]} for u in sorted(leaderboard, key=leaderboard.get, reverse=True)]
+
+    def get_by_range(self, of_date: pendulum.DateTime, unit: str):
+        start_time = of_date.start_of(unit).timestamp()
+        end_time = of_date.end_of(unit).timestamp()
+
+        return self.calculate_score(self.get_data(start_time, end_time))
+
+    def get_week(self, of_date: pendulum.DateTime):
+        return self.get_by_range(of_date, 'week')
+
+    def get_month(self, of_date: pendulum.DateTime):
+        return self.get_by_range(of_date, 'month')
+
+
--- plusplusbot/tests/analytics/leaderboard.py
+++ plusplusbot/tests/analytics/leaderboard.py
@@ -1,19 +1,35 @@
+import pendulum
+
 from plusplusbot.analytics.leaderboard import LeaderBoard
 import pytest
 import json
 
 
 class TestLeaderBoard:
-
     @pytest.fixture
     def lb(self):
         history = json.load(open("../fixtures/history.json"))
         return LeaderBoard(history)
 
+    @pytest.fixture
+    def current_date(self):
+        return pendulum.datetime(2020, 6, 20, tz=pendulum.timezone('Australia/Melbourne'))
+
     def test_fixture_loading(self, lb):
         assert len(lb.history) > 0
 
-    def test_get_week(self):
-        lb = LeaderBoard([])
+    def test_get_week(self, lb, current_date):
+        assert lb.get_week(current_date) == [
+            {'U985L6R1M': 15},
+            {'U0VCW825A': 13},
+            {'U5HKU1Q0W': 12},
+            {'U0ZC11HC7': 9}
+        ]
 
-        assert lb.get_week() == 100
+    def test_get_month(self, lb, current_date):
+        assert lb.get_month(current_date) == [
+            {'U0VCW825A': 44},
+            {'U5HKU1Q0W': 43},
+            {'U985L6R1M': 42},
+            {'U0ZC11HC7': 33}
+        ]
