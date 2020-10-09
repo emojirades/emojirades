fb3e878b0a7ec42bfc7f7376fb26b1ac3b298d7b
Michael Robbins <michael@dalmura.com.au>
2020-10-09 15:21:54 +1100

    Implementing a dense rank for our leaderboards

--- emojirades/printers/leaderboard_printer.py
+++ emojirades/printers/leaderboard_printer.py
@@ -17,8 +17,12 @@ class LeaderboardPrinter:
     def print_date_range(self):
         date_range = ""
         if self.time_unit in [TimeUnit.WEEKLY, TimeUnit.MONTHLY]:
-            start = TimeRange.get_start_date(self.of_date, self.time_unit).format("YYYY-MM-DD")
-            end = TimeRange.get_end_date(self.of_date, self.time_unit).format("YYYY-MM-DD")
+            start = TimeRange.get_start_date(self.of_date, self.time_unit).format(
+                "YYYY-MM-DD"
+            )
+            end = TimeRange.get_end_date(self.of_date, self.time_unit).format(
+                "YYYY-MM-DD"
+            )
             date_range = f"({start} - {end})"
 
         return date_range
@@ -44,10 +48,7 @@ class LeaderboardPrinter:
             yield None, "Nothing to see here!"
             return
 
-        lines = [
-            "```",
-            self.print_title(),
-            ""]
+        lines = ["```", self.print_title(), ""]
 
         longest_name = 0
         biggest_score = 0
@@ -64,12 +65,22 @@ class LeaderboardPrinter:
             if score_length > biggest_score:
                 biggest_score = score_length
 
+        rank_buffer = 0
+        last_score = 0
+
         for index, (name, score) in enumerate(leaderboard, start=1):
             name = self.slack.pretty_name(name)
             name = name if len(name) < 20 else "{name[0:18]}.."
 
+            if score == last_score:
+                rank_buffer -= 1
+            elif rank_buffer < 0:
+                rank_buffer = 0
+
+            last_score = score
+
             lines.append(
-                f"{index:>2}. {name:<{longest_name}} [ {score:>{biggest_score}} point{'s' if score > 1 else ' '} ]"
+                f"{index + rank_buffer:>2}. {name:<{longest_name}} [ {score:>{biggest_score}} point{'s' if score > 1 else ' '} ]"
             )
 
         lines.append("```")
