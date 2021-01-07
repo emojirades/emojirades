21b229cc31d34ecb115da253ecc7197b9898dfb4
Michael Robbins <michael+github@dalmura.com.au>
2021-01-07 22:39:30 +1100

    Bumping license metadata in setup.py to the current license, code already licensed a while back

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url="https://github.com/emojirades/emojirades",
     classifiers=[
         "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
