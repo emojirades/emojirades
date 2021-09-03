ac04c0b8b8c90d6fc1c8b4066663c7206fe043b8
Michael Robbins <michael+github@dalmura.com.au>
2021-09-03 19:12:58 +1000

    Removing slack_ws debug msgs and updating CI to use slack_ws.py

--- tests/slack_ws.py
+++ tests/slack_ws.py
@@ -7,15 +7,15 @@ import asyncio
 from websockets import serve
 
 
-logger = logging.getLogger("websockets.server")
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler())
+#logger = logging.getLogger("websockets.server")
+#logger.setLevel(logging.DEBUG)
+#logger.addHandler(logging.StreamHandler())
 
 
 async def echo(websocket, path):
     async for message in websocket:
         await websocket.send(message)
-        print(message)
+        #print(message)
 
 
 async def main():
@@ -23,5 +23,5 @@ async def main():
         await asyncio.Future()
 
 
-logger.info("Starting ws://localhost:8765/")
+#logger.info("Starting ws://localhost:8765/")
 asyncio.run(main())
