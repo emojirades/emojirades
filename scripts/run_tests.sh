61391c5fa5db1f5b7ab1083f0e985780eefb470c
Michael Robbins <michael+github@dalmura.com.au>
2021-09-05 17:58:39 +1000

    Fold all pytest into one

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,12 +3,6 @@
 ./tests/slack_ws.py &
 WS_PID=$!
 
-pytest tests/test_commands.py
-pytest tests/test_event.py
-pytest tests/test_gamestate.py
-pytest tests/test_leaderboard.py
-pytest tests/test_scenarios.py
-pytest tests/test_scorekeeper.py
-pytest tests/test_time_range.py
+pytest
 
 kill "${WS_PID}"
