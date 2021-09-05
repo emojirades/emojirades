4939f47b80b3128ad1d0c1e95bed4f57193ef09c
Michael Robbins <michael+github@dalmura.com.au>
2021-09-05 17:09:56 +1000

    Run each test file separtely

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,6 +3,12 @@
 ./tests/slack_ws.py &
 WS_PID=$!
 
-pytest
+pytest tests/test_commands.py
+pytest tests/test_event.py
+pytest tests/test_gamestate.py
+pytest tests/test_leaderboard.py
+pytest tests/test_scenarios.py
+pytest tests/test_scorekeeper.py
+pytest tests/test_time_range.py
 
 kill "${WS_PID}"
